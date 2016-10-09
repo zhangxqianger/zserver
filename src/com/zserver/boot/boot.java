91ece099f06e8f1b112aec1b2e290c77b77a8598
zhangxqianger <zhangxqianger@vip.qq.com>
2016-10-09 17:29:53 +0800

    测试

---
+++ src/com/zserver/boot/boot.java
@@ -0,0 +1,12 @@
+package com.zserver.boot;
+
+/**
+ * Created by Administrator on 2016/10/9.
+ * 启动程序
+ */
+public class boot {
+    public static void main(String[] args) {
+        // 开始项目了
+
+    }
+}
